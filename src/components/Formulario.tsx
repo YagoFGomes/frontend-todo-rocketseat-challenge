3d916434435fb274711b5e0cad93377966e07484
Yago Ferreira <yago.fgomes@gmail.com>
2024-02-20 20:39:57 +0000

    fix button

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { toast } from "sonner";
 
 interface Props {
   title: string;
@@ -9,11 +10,23 @@ function Formulario({ title, subtitle }: Props) {
   const [taskTitle, setTeskTitle] = useState<string>("");
   const [taskDescription, setTeskDescription] = useState<string>("");
 
+  function checkFields() {
+    if (
+      taskTitle !== "" &&
+      taskTitle.trim().length > 0 &&
+      taskDescription !== "" &&
+      taskDescription.trim().length > 0
+    ) {
+      return "";
+    }
+    return "disabled";
+  }
+
   async function handleSubmit() {
     if (
-      taskTitle &&
+      taskTitle !== "" &&
       taskTitle.trim().length > 0 &&
-      taskDescription &&
+      taskDescription !== "" &&
       taskDescription.trim().length > 0
     ) {
       try {
@@ -32,19 +45,18 @@ function Formulario({ title, subtitle }: Props) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        const data = await response.json();
-        console.log("Task created:", data);
-
         // Handle the response data as needed...
       } catch (error) {
         console.error("Error creating task:", error);
         // Handle errors as needed...
       }
+    } else {
+      toast.error("Fields are required");
     }
   }
 
   return (
-    <form>
+    <div>
       <h1 className="text-4xl font-bold mb-2">{title}</h1>
       <p className="mb-8">{subtitle}</p>
       <div className="mb-6">
@@ -54,22 +66,24 @@ function Formulario({ title, subtitle }: Props) {
           className="w-full p-3 mb-4 bg-gray-700 rounded"
           value={taskTitle}
           onChange={(e) => setTeskTitle(e.target.value)}
+          required
         />
         <textarea
           placeholder="Description"
           className="w-full p-3 mb-4 bg-gray-700 rounded"
           value={taskDescription}
           onChange={(e) => setTeskDescription(e.target.value)}
+          required
         ></textarea>
         <button
           onClick={handleSubmit}
-          className="group w-full p-3 bg-purple-800 rounded hover:bg-purple-700 transition-all font-semibold text-md"
+          className={`group w-full p-3 bg-purple-800 rounded hover:bg-purple-700 transition-all font-semibold text-md ${checkFields}`}
         >
           Submit{"  "}
           <span className="inline-block group-hover:animate-bounce ">🚀</span>
         </button>
       </div>
-    </form>
+    </div>
   );
 }
 
